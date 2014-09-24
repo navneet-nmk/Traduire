55157b9069b3a7c8aaa41ac9e31df7540a26a2e5
Navneet M Kumar <navneet.nmk@gmail.com>
2014-09-24 14:02:54 +0530

    Added translation activity using Bing API

--- gen/com/example/traduire/R.java
+++ gen/com/example/traduire/R.java
@@ -18,11 +18,12 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f080004;
@@ -38,12 +39,13 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int takePictureButton=0x7f050003;
-        public static final int take_photo_text=0x7f050005;
-        public static final int translatebutton=0x7f050004;
+        public static final int action_settings=0x7f040003;
+        public static final int app_name=0x7f040001;
+        public static final int hello=0x7f040000;
+        public static final int hello_world=0x7f040002;
+        public static final int takePictureButton=0x7f040004;
+        public static final int take_photo_text=0x7f040006;
+        public static final int translatebutton=0x7f040005;
     }
     public static final class style {
         /** 
--- src/com/example/traduire/MainActivity.java
+++ src/com/example/traduire/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.traduire;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -16,25 +21,82 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
 
 public class MainActivity extends Activity {
 	protected Button takePictureButton;
 	protected Button translateButton;
 	protected ImageView uploadedImage;
+	protected TextView textUpload;
 	protected String path;
 	protected String path_two;
 	protected Uri mMediaUri;
+	// Path for storing the language file at for use in Tesseract
+	public static final String DATA_PATH = Environment
+			.getExternalStorageDirectory().toString() + "/Traduire/";
+	// Defining the language as english
+	public static final String lang = "eng";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// Checking the paths to check for the existence of the directories.
+		String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+		for (String path : paths) {
+			File dir = new File(path);
+			if (!dir.exists()) {
+				if (!dir.mkdirs()) {
+					// Log.v(TAG, "ERROR: Creation of directory " + path +
+					// " on sdcard failed");
+					return;
+				} else {
+					// Log.v(TAG, "Created directory " + path + " on sdcard");
+				}
+			}
+
+		}
+		if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata"))
+				.exists()) {
+			try {
+				// For language files stored in assets.
+				AssetManager assetManager = getAssets();
+				InputStream in = assetManager.open("tessdata/" + lang
+						+ ".traineddata");
+				OutputStream out = new FileOutputStream(DATA_PATH + "tessdata/"
+						+ lang + ".traineddata");
+
+				// Transfer bytes from in to out
+				byte[] buf = new byte[1024];
+				int len;
+				// while ((lenf = gin.read(buff)) > 0) {
+				while ((len = in.read(buf)) > 0) {
+					out.write(buf, 0, len);
+				}
+				in.close();
+				// gin.close();
+				out.close();
+
+				// Log.v(TAG, "Copied " + lang + " traineddata");
+			} catch (IOException e) {
+				// Log.e(TAG,
+				// "Was unable to copy " + lang + " traineddata "
+				// + e.toString());
+			}
+		}
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// Declare the variables
+		textUpload = (TextView) findViewById(R.id.textView1);
 		uploadedImage = (ImageView) findViewById(R.id.uploadedImage);
 		takePictureButton = (Button) findViewById(R.id.takePicture);
 		translateButton = (Button) findViewById(R.id.translateButton);
-		path = Environment.getExternalStorageDirectory()
-				+ "/translate.jpg";
+		path = DATA_PATH + "/ocr.jpg";
+
 		takePictureButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
@@ -70,18 +132,44 @@ public class MainActivity extends Activity {
 			break;
 
 		case -1:
-			onPhotoTaken();
+			try {
+				onPhotoTaken();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			break;
 		}
 	}
 
-	protected void onPhotoTaken() {
+	protected void onPhotoTaken() throws Exception {
 
 		BitmapFactory.Options options = new BitmapFactory.Options();
 		options.inSampleSize = 4;
 
 		Bitmap bitmap = BitmapFactory.decodeFile(path, options);
 		uploadedImage.setImageBitmap(bitmap);
+		// Required by tess
+		bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+		TessBaseAPI baseAPI = new TessBaseAPI();
+		baseAPI.setDebug(true);
+		baseAPI.init(DATA_PATH, lang);
+		baseAPI.setImage(bitmap);
+		String text = baseAPI.getUTF8Text();
+		textUpload.setText(text);
+		String translatedText = translate(text);
+		textUpload.setText(translatedText);
+
+	}
+
+	// Define function to translate the string
+	protected String translate(String text) throws Exception {
+		Translate.setClientId("NavneetKumarTranslator");
+		Translate
+				.setClientSecret("csrDHAaipBK9baYP5L2IQVl2TLEactdq7WM0Unffano");
+		String translatedtext = "";
+		translatedtext = Translate.execute(text, Language.GERMAN);
+		return translatedtext;
 
 	}
 
